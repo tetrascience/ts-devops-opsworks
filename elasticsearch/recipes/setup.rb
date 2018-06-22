0a49da69a5da813644f2673219e576438a4c1c40
Brayan Lemus <brayest3@gmail.com>
2018-06-22 00:13:21 -0600

    fixes

--- elasticsearch/recipes/setup.rb
+++ elasticsearch/recipes/setup.rb
@@ -62,7 +62,7 @@ directory '/tmp/ssm' do
   action :create
 end
 
-remote_file '/tmp/ssm' do
+remote_file '/tmp/ssm/amazon-ssm-agent.deb' do
   source 'https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb'
   owner 'root'
   group 'root'
