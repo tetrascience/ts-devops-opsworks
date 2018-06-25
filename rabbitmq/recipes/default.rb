f461932da3798ae28607c210358a620b11744748
Brayan Lemus <brayest3@gmail.com>
2018-06-25 15:36:22 -0600

    erlang update

--- rabbitmq/recipes/default.rb
+++ rabbitmq/recipes/default.rb
@@ -19,7 +19,7 @@ end
 
 package ['erlang-base','erlang-inets', 'erlang-crypto', 'erlang-inets', 'erlang-runtime-tools', 'erlang-snmp', 'erlang-ssh', 'erlang-tools', 'erlang-xmerl', 'erlang-syntax-tools', 'erlang-ic', 'erlang-mnesia', 'erlang-ssl', 'erlang-public-key', 'erlang-asn1'] do
   action :install
-  version ['1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1',, '1:20.3-1']
+  version ['1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1', '1:20.3-1']
   timeout 900
 end
 
